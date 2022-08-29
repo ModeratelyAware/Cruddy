dffc6de82486c1246e7e8221f724dad39013fbb9
Steven Karatisoglou <lkytiger@outlook.com>
2022-08-29 09:24:21 -0700

    Mics comment cleanup.

--- ApplicationCore/Queries/QuerySpecificationExtensions.cs
+++ ApplicationCore/Queries/QuerySpecificationExtensions.cs
@@ -6,17 +6,14 @@ public static class QuerySpecificationExtensions
 {
 	public static IQueryable<T> Specify<T>(this IQueryable<T> query, ISpecification<T> spec) where T : class
 	{
-		// fetch a Queryable that includes all expression-based includes
 		var queryableResultWithIncludes = spec.Includes
 			.Aggregate(query,
 				(current, include) => current.Include(include));
 
-		// modify the IQueryable to include any string-based include statements
 		var secondaryResult = spec.IncludeStrings
 			.Aggregate(queryableResultWithIncludes,
 				(current, include) => current.Include(include));
 
-		// return the result of the query using the specification's criteria expression
 		if (spec.Criteria == null)
 		{
 			return secondaryResult;
@@ -27,17 +24,13 @@ public static class QuerySpecificationExtensions
 
 	public static IOrderedQueryable<T> SpecifyOrderBy<T>(this IQueryable<T> query, ISpecification<T> spec) where T : class
 	{
-		// fetch a Queryable that includes all expression-based includes
 		var queryableResultWithIncludes = spec.Includes
 			.Aggregate(query,
 				(current, include) => current.Include(include));
 
-		// modify the IQueryable to include any string-based include statements
 		var secondaryResult = spec.IncludeStrings
 			.Aggregate(queryableResultWithIncludes,
 				(current, include) => current.Include(include));
-
-		// return the result of the query using the specification's criteria expression
 
 		if (spec.OrderBy == null)
 		{
